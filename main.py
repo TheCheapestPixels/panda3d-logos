610b2f136b7ebf169382abcde75f5d22bd92796f
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2020-02-19 05:57:40 +0100

    Even more assorted tinkery.

--- main.py
+++ main.py
@@ -1,45 +1,69 @@
 import sys
+
 from direct.showbase.ShowBase import ShowBase
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
+from direct.interval.IntervalGlobal import Parallel
+from direct.interval.IntervalGlobal import LerpPosHprInterval
 from direct.interval.IntervalGlobal import LerpFunc
 from panda3d.core import loadPrcFileData
 from panda3d.core import AntialiasAttrib
+from panda3d.core import Shader
+from panda3d.core import Vec3
+
 
 loadPrcFileData('', 'fullscreen true')
 loadPrcFileData('', 'framebuffer-multisample 1')
 loadPrcFileData('', 'multisamples 2')
+
 
 class SplashBase(ShowBase):
     def __init__(self):
         ShowBase.__init__(self) 
         self.accept('escape', sys.exit)
         base.win.set_clear_color((0,0,0,1))
-        base.cam.set_pos(0, 1, 0.1)
-        #base.cam.look_at(0, 6.3, -0.1)
-        #base.cam.set_pos(0, -50, 0)
+        base.cam.set_pos(0, -2, 0)
 
         # Load and prepare content
         self.logo_animation = Actor("panda3d_logo.bam")  # Centered around 0, 6.3, 0
         self.logo_animation.reparent_to(render)
-        render.set_antialias(AntialiasAttrib.MMultisample)
+        self.logo_animation.set_pos(0, -3.15, -0.1)
+        shader = Shader.load(
+            Shader.SL_GLSL,
+            vertex="panda3d_logo.vert",
+            fragment="panda3d_logo.frag",
+        )
+        self.logo_animation.set_shader(shader)
+        self.logo_animation.set_shader_input("fade", 0.0)
         self.logo_sound = base.loader.loadSfx("panda3d_logo.wav")
-        def null_effect(t):
+        def null_func(t):
             pass
+        def shader_time(t):
+            self.logo_animation.set_shader_input("time", t)
+        def add_antialiasing(t):
+            render.set_antialias(AntialiasAttrib.MMultisample)
+            print(".")
         def fade_background_to_white(t):
             base.win.set_clear_color((t,t,t,1))
+            self.logo_animation.set_shader_input("fade", t)
         self.effects = Sequence(
             LerpFunc(
-                null_effect,
+                shader_time,
                 fromData=0,
                 toData=1,
                 duration=3.878,
             ),
             LerpFunc(
+                add_antialiasing,
+                fromData=0,
+                toData=1,
+                duration=0,
+            ),
+            LerpFunc(
                 fade_background_to_white,
                 fromData=0,
                 toData=1,
-                duration=1,
+                duration=1.0,
             ),
         )
 
