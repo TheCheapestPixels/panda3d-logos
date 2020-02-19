d35138384aa482f0d1d8266110395d50a2dd7a46
momojohobo <momojo@rocketship.com>
2020-02-19 20:53:12 +0100

    torus-shaped explosion, added requirements.txt

--- panda3d_logo.py
+++ panda3d_logo.py
@@ -1,4 +1,5 @@
 import sys
+#import simplepbr
 from direct.showbase.ShowBase import ShowBase
 from direct.actor.Actor import Actor
 
@@ -8,18 +9,18 @@ class SplashBase(ShowBase):
         ShowBase.__init__(self) 
         self.accept('escape', sys.exit)
         base.win.set_clear_color((0,0,0,1))
+        #simplepbr.init()
 
         self.logo_animation = Actor("panda3d_logo.bam")
         self.logo_animation.reparent_to(render)
         self.logo_animation.play("splash")
-        print("currently playing: " + self.logo_animation.getCurrentAnim())
+        self.logo_animation.set_two_sided(True)
 
         self.logo_sound = base.loader.loadSfx("panda3d_logo.wav")
         self.logo_sound.play()
 
         render.ls()
         render.analyze()
-
 
 app = SplashBase()
 app.run()
